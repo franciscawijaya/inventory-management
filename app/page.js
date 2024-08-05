e055cbbceb6abcfd18a7534aa809787d8845b348
franciscawijaya <fwijaya@wesleyan.edu>
2024-08-05 12:11:47 +0700

    Fix SSR issues with Firebase initialization

--- app/page.js
+++ app/page.js
@@ -1,61 +1,97 @@
+// 'use client'
+// import Image from "next/image";
+// import {useState, useEffect} from "react";
+// import {firestore} from "@/firebase";
+// import {Box, Modal, Typography, Stack, TextField, Button} from "@mui/material";
+// import {collection, query, getDocs, doc, setDoc, getDoc, deleteDoc} from "firebase/firestore";
+
+// export default function Home() {
+//   const [inventory, setInventory] = useState([]);
+//   const [open, setOpen] = useState(true);
+//   const [itemName, setItemName] = useState('');
+  
+//   const updateInventory = async () => {
+//     const snapshot = query(collection(firestore, "inventory"));
+//     const docs = await getDocs(snapshot);
+//     const inventoryList = [];
+//     docs.forEach((doc) => {
+//       inventoryList.push({
+//         name: doc.id,
+//         ...doc.data(),
+//       });
+//     });
+//     setInventory(inventoryList);
+//   }
+
+//   const addItem = async (item) => {
+//     const docRef = doc(collection(firestore, "inventory"), item);
+//     const docSnap = await getDoc(docRef);
+
+//     if (docSnap.exists()) {
+//       const {quantity} = docSnap.data();
+//       await setDoc(docRef, {quantity: quantity + 1});
+//     }
+//     else {
+//       await setDoc(docRef, {quantity: 1});
+//     }
+//    await updateInventory();
+//   }
+  
+
+//   const removeItem = async (item) => {
+//     const docRef = doc(collection(firestore, "inventory"), item);
+//     const docSnap = await getDoc(docRef);
+
+//     if (docSnap.exists()) {
+//       const {quantity} = docSnap.data();
+//       if (quantity === 1) {
+//         await deleteDoc(docRef);
+//       } else {
+//         await setDoc(docRef, {quantity: quantity - 1});
+//       }
+//     }
+//    await updateInventory();
+//   }
+
+//   useEffect(() => {
+//     updateInventory()
+//   }, [])
+
 'use client'
 import Image from "next/image";
 import {useState, useEffect} from "react";
-import {firestore} from "@/firebase";
 import {Box, Modal, Typography, Stack, TextField, Button} from "@mui/material";
-import {collection, query, getDocs, doc, setDoc, getDoc, deleteDoc} from "firebase/firestore";
 
 export default function Home() {
   const [inventory, setInventory] = useState([]);
   const [open, setOpen] = useState(true);
   const [itemName, setItemName] = useState('');
-  
-  const updateInventory = async () => {
-    const snapshot = query(collection(firestore, "inventory"));
-    const docs = await getDocs(snapshot);
-    const inventoryList = [];
-    docs.forEach((doc) => {
-      inventoryList.push({
-        name: doc.id,
-        ...doc.data(),
-      });
-    });
-    setInventory(inventoryList);
-  }
-
-  const addItem = async (item) => {
-    const docRef = doc(collection(firestore, "inventory"), item);
-    const docSnap = await getDoc(docRef);
-
-    if (docSnap.exists()) {
-      const {quantity} = docSnap.data();
-      await setDoc(docRef, {quantity: quantity + 1});
-    }
-    else {
-      await setDoc(docRef, {quantity: 1});
-    }
-   await updateInventory();
-  }
-  
-
-  const removeItem = async (item) => {
-    const docRef = doc(collection(firestore, "inventory"), item);
-    const docSnap = await getDoc(docRef);
-
-    if (docSnap.exists()) {
-      const {quantity} = docSnap.data();
-      if (quantity === 1) {
-        await deleteDoc(docRef);
-      } else {
-        await setDoc(docRef, {quantity: quantity - 1});
-      }
-    }
-   await updateInventory();
-  }
+  const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
-    updateInventory()
-  }, [])
+    setIsClient(true);
+    const initFirebase = async () => {
+      const { firestore } = await import("@/firebase");
+      const { collection, query, getDocs, doc, setDoc, getDoc, deleteDoc } = await import("firebase/firestore");
+
+      const updateInventory = async () => {
+        const snapshot = query(collection(firestore, "inventory"));
+        const docs = await getDocs(snapshot);
+        const inventoryList = [];
+        docs.forEach((doc) => {
+          inventoryList.push({
+            name: doc.id,
+            ...doc.data(),
+          });
+        });
+        setInventory(inventoryList);
+      }
+
+      updateInventory();
+    }
+
+    initFirebase();
+  }, []);
 
   const handleOpen = () => 
     setOpen(true);
